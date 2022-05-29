90da0a7196f32c50923c2fa24b31576807e4567d
Luis Garcia <bigluisbad@gmail.com>
2022-05-29 18:29:42 -0400

    funcion handleChange en formulario

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const TaskForm = () => {
+
+  const[ task, setTask] = useState({
+    title: "",
+    description: "",
+  })
+  const handleChange = (e)=>{
+    setTask({...task, [e.target.name]: e.target.value});
+  }
+
   return (
     <div className=' form'>
       <h2>A task</h2>
       <form className='form-c'>
-        <input type="text" placeholder='Write Title' className='input' />
-        <textarea cols="20" rows="5" placeholder='Writer description' className='input'/>
+        <input 
+          type="text"
+          name='title'
+          placeholder='Write Title'
+          onChange={handleChange}
+          className='input' />
+        <textarea 
+          cols="20"
+          rows="5"
+          placeholder='Writer description'
+          name='description'
+          onChange={handleChange}
+          className='input'/>
         <button className='button'>Create Task</button>
       </form>
     </div>
